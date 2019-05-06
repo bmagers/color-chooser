d7361741780cc1bac511b0585c993df4af6f7c60
Bryan Magers <bmagers@users.noreply.github.com>
2019-05-06 09:44:57 -0700

    Updates

---
+++ precache-manifest.e5f03904551de06cd32e1e5c21998873.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a70b90849066d189d2047d26bd80495",
+    "url": "/color-chooser/index.html"
+  },
+  {
+    "revision": "474602ebe0154706ebdc",
+    "url": "/color-chooser/static/css/main.aa3801f8.chunk.css"
+  },
+  {
+    "revision": "401e1611a9fb07a2b384",
+    "url": "/color-chooser/static/js/2.f9b54071.chunk.js"
+  },
+  {
+    "revision": "474602ebe0154706ebdc",
+    "url": "/color-chooser/static/js/main.93b8851b.chunk.js"
+  },
+  {
+    "revision": "fa42f76e7e6b80ff0849",
+    "url": "/color-chooser/static/js/runtime~main.46ca822b.js"
+  }
+]);
